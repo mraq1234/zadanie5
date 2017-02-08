fd48aecad4b09c2d87faf2ffba0fd2eeb5912057
Mariusz <Mariusz>
2017-02-08 18:41:07 +0100

    dodana obsługa błędów

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 var http = require('http');
 var fs = require('fs');
 var server = http.createServer();
+
+function errMsgToBrowser(response) {
+    response.statusCode = 404;
+    response.write('<img src="http://shaebaxter.com/wp-content/uploads/2014/06/404-error-page-example.jpg">');
+    response.end();
+}
+
 server.on('request', function (request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
     if (request.method === 'GET' && request.url === '/') {
-        fs.readFile('index.html', 'utf-8', function(err, data){
+        fs.readFile('index.html', 'utf-8', function (err, data) {
             if (err) {
-                throw err;
+                console.error('wystąpił problem podczas odczytu pliku: ', err.message);
+                errMsgToBrowser(response);
+            } else {
+                response.write(data);
+                response.end();
             }
-            response.write(data);
-            response.end();
-        });    
+        });
     } else {
-            response.statusCode = 404;
-            response.write('<img src="http://shaebaxter.com/wp-content/uploads/2014/06/404-error-page-example.jpg">');
-            response.end();
+        errMsgToBrowser(response);
     }
 });
 server.listen(9000);
